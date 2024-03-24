2bb77e716521aa5e16ed0cfed64c0095f8e1a0cc
Stepan Afanasov <asvtep482@gmail.com>
2024-03-24 23:51:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 10_000;
+        int rub = 20;
+        int bonus = (cost / rub);
+        System.out.println(bonus);
+
+    }
+}
